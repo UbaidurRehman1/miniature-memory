f66201f279976af984347839bd2b1fcfcf799824
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-03-06 13:25:01 +0500

    api gateway server add

--- todo-ms/src/main/java/com/codemaster/todoms/TodoMsApplication.java
+++ todo-ms/src/main/java/com/codemaster/todoms/TodoMsApplication.java
@@ -2,8 +2,10 @@ package com.codemaster.todoms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TodoMsApplication {
 
 	public static void main(String[] args) {
