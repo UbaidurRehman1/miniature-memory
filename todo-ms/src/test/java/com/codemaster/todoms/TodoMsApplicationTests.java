f06d2581891f34450bf26f06be0a33f48c170a42
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-03-06 12:55:03 +0500

    services add

---
+++ todo-ms/src/test/java/com/codemaster/todoms/TodoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemaster.todoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
